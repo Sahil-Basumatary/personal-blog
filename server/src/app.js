4055e81ae12f18ec1501a383a525f7af0761b3c1
Sahil Basumatary <Basumatarysahil03@gmail.com>
2025-11-10 03:30:49 +0000

    add mock route for testing

--- server/src/app.js
+++ server/src/app.js
@@ -15,5 +15,5 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
