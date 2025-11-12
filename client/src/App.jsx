0553242b5fba4bae406f2ae794b3437789e1c969
Sahil Basumatary <Basumatarysahil03@gmail.com>
2025-11-12 16:58:29 +0000

    feat: fetch mock posts from backend API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+   //basic for now :)
+    fetch("http://localhost:5050/api/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error("Error fetching posts:", err));
+  }, []);
 
   return (
-    <>
-      <div>
-        Sahils Blog
-      </div>
-    </>
-  )
+    <div style={{ padding: "2rem" }}>
+      <h1>Sahils Blog</h1>
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post.id} style={{ marginBottom: "1rem" }}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))
+      ) : (
+        <p>Cooking posts....</p>
+      )}
+    </div>
+  );
 }
 
 export default App
