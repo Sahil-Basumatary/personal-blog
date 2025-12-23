e4e86d8027ff905cb6f1659e721976e25714940b
Sahil Basumatary <Basumatarysahil03@gmail.com>
2025-12-23 15:14:55 +0000

    strengthen posts API coverage

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,23 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB = async (
-  mongoUri = process.env.MONGODB_URI,
-  { isTest = false } = {}
-) => {
+const connectDB = async (uri, { isTest = false } = {}) => {
   try {
+    const mongoUri = uri || process.env.MONGODB_URI;
+
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI is not set");
+    }
+
     await mongoose.connect(mongoUri);
+
     if (!isTest) {
       console.log("MongoDB connected");
     }
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
 
-    if (isTest || process.env.NODE_ENV === "test") {
+    if (isTest) {
       throw err;
+    } else {
+      process.exit(1);
     }
-
-    process.exit(1);
   }
 };
 
