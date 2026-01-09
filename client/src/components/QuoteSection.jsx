d6637fbba89413020e8d21d5b7991f8430998824
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-01-09 09:56:54 +0000

    update quote section + fix sitemap link

--- client/src/components/QuoteSection.jsx
+++ client/src/components/QuoteSection.jsx
@@ -5,8 +5,7 @@ function QuoteSection() {
     <section className="quote-section">
       <p className="quote">
         If she values you and your time, no effort will be required.
-        If she doesn't give a damn about your presence and time,
-        no amount of effort will change that.
+        If she doesn't, no amount of effort will change that.
       </p>
     </section>
   );
