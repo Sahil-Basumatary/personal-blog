4794d9694ca1794e596d73293e75ca1eb410e4fc
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-01-16 15:16:01 +0000

    setup seo config and update package.json

---
+++ client/src/config/seo.js
@@ -0,0 +1,9 @@
+const baseUrl = import.meta.env.VITE_SITE_URL || "https://blog.sahilbzy.com";
+export const SEO = {
+  siteName: "Sahil Blogs",
+  baseUrl,
+  defaultImage: `${baseUrl}/images/original-logo.png`,
+  twitterHandle: "@sahilbzy",
+  author: "Sahil Basumatary",
+};
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
-
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -14,10 +14,12 @@ if (!clerkPubKey) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+    <HelmetProvider>
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ClerkProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
